912b4512df77b0df0c0ee8c4d0d12e737a667e50
Jayfunf <simh3077@gmail.com>
2025-03-26 02:20:56 +0900

    init

---
+++ STCRUD/src/test/java/MINION/STCRUD/StcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package MINION.STCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
